b911676fc5bfb3273df3ee9aa01545855fac0eba
Johanne <johanne_lewis@hotmail.com>
2019-09-22 13:53:35 +0100

    Add other animation effects on content

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -27,4 +27,22 @@ $(document).ready(function(){
     }, {
         offset: '50%'
     });
+
+    $('.js--waypoint-2').waypoint(function(direction){
+        $('.js--waypoint-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--waypoint-3').waypoint(function(direction){
+        $('.js--waypoint-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--waypoint-4').waypoint(function(direction){
+        $('.js--waypoint-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
 });
