24cfa1d4b7473cf547eb9bc5c9ca2816f79b371e
Johanne <johanne_lewis@hotmail.com>
2019-09-22 13:40:44 +0100

    Script for animating features section on scroll

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -21,4 +21,8 @@ $(document).ready(function(){
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     });
 
+    /** Animate scroll */
+    $('.js--waypoint-1').waypoint(function(direction){
+        $('.js--waypoint-1').addClass('animated fadeIn');
+    });
 });
